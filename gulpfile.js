7bf45629af85ef48002508d2ae1b36377f26d470
Saigesp <saigesp@saigesp.es>
2016-06-25 18:43:16 +0200

    instal npm, gulp, configure gulp-less, install flexboxer

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+
+var gulp       = require('gulp');  
+var less       = require('gulp-less');  
+var watch      = require('gulp-watch');  
+var minifyCSS  = require('gulp-minify-css');  
+var rename     = require('gulp-rename');  
+var header     = require('gulp-header');  
+var pkg        = require('./package.json');
+
+/* Prepare banner text */
+var banner = ['/**',  
+  ' * Project: <%= pkg.name %> ',
+  ' * Version: v<%= pkg.version %>.2',
+  ' * Description: <%= pkg.description %>',
+  ' * Author: <%= pkg.author.name %>',
+  ' * Author email: <%= pkg.author.email %>',
+  ' * Licence: <%= pkg.license %>',
+  ' * Web: <%= pkg.homepage %>',
+  ' */',
+  ''].join('\n');
+
+/* Task to compile less */
+gulp.task('less', function() {  
+  gulp.src('./less/dev/style.less')
+    .pipe(less())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('./assets/styles/'));
+});
+
+/* Task to watch less changes */
+gulp.task('watch-less', ['less'], function() {  
+  gulp.watch('./dev/*.less' , ['less']);
+});
+
+/* Task to minify css */
+gulp.task('min', ['less'], function() {  
+  gulp.src('./assets/styles/*.css')
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest( './assets/styles/min/' ));
+});
+
+/* Task when running `gulp` from terminal */
+gulp.task('serve', ['watch-less']);
+
+/* Task when running `gulp build` from terminal */
+gulp.task('build', ['min']);  
