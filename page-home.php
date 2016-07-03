90850d3a6227966ace42802bebe97a0b67e9db93
Saigesp <saigesp@gmail.com>
2016-07-03 21:31:31 +0200

    adjust some elements, prepare for others

--- page-home.php
+++ page-home.php
@@ -5,5 +5,12 @@
 
     <div id="chart"></div>
 
+    <section class="wrap wrap--content text--center wrap--spaced">
+    	<h1>V Sevilla Ciudad del Diseño</h1>
+    	<p>La SCD es el principal evento de diseño de producto de Andalucía, un encuentro que reúne a estudiantes, profesionales y organismos del mundo del diseño de todo el Estado.</p>
+		<p>La <strong>5ª edición</strong> tendrá lugar en Sevilla en la <strong>primavera de 2017</strong>.</p>
+
+    </section>
+
   </div><!-- end of flexboxer -->
 <?php get_footer(); ?>
