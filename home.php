5abc59df9e5d8e34daf605c686e670a6e7c39b82
Saigesp <saigesp@saigesp.es>
2016-07-04 04:48:14 +0200

    automatize plugin & dependencies injection

--- home.php
+++ home.php
@@ -5,12 +5,16 @@
 
     <div id="chart"></div>
 
-    <section class="wrap wrap--content text--center wrap--spaced">
+    <section class="wrap wrap--content text--center wrap--spaced" style="margin-top:0 !important;">
     	<h1>V Sevilla Ciudad del Diseño</h1>
     	<p>La SCD es el <strong>evento de diseño de producto más importante de Andalucía</strong>, un encuentro anual que reúne a <strong>estudiantes, profesionales y organismos</strong> de todo el Estado y que supone una oportunidad única para intercambiar conocimientos e ideas con los profesionales y académicos más destacados del sector.</p>
 		<p>La <strong>5ª edición</strong> tendrá lugar en Sevilla en la <strong>primavera de 2017</strong>.</p>
 		<p><strong>¿Te esperamos?</strong></p>
+    </section>
 
+    <section class="wrap wrap--content wrap--spaced hidden">
+    	<h1>Ubicación</h1>
+		<p>La <strong>5ª edición</strong> tendrá lugar en Sevilla en la <strong>primavera de 2017</strong>.</p>
     </section>
 
   </div><!-- end of flexboxer -->
