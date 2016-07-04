2c555b33e63860e1c311bbfaa563d4a61f715392
Saigesp <saigesp@saigesp.es>
2016-07-04 04:02:39 +0200

    Update logo & claim

--- home.php
+++ home.php
@@ -7,8 +7,9 @@
 
     <section class="wrap wrap--content text--center wrap--spaced">
     	<h1>V Sevilla Ciudad del Diseño</h1>
-    	<p>La SCD es el principal evento de diseño de producto de Andalucía, un encuentro que reúne a estudiantes, profesionales y organismos del mundo del diseño de todo el Estado.</p>
+    	<p>La SCD es el <strong>evento de diseño de producto más importante de Andalucía</strong>, un encuentro anual que reúne a <strong>estudiantes, profesionales y organismos</strong> de todo el Estado y que supone una oportunidad única para intercambiar conocimientos e ideas con los profesionales y académicos más destacados del sector.</p>
 		<p>La <strong>5ª edición</strong> tendrá lugar en Sevilla en la <strong>primavera de 2017</strong>.</p>
+		<p><strong>¿Te esperamos?</strong></p>
 
     </section>
 
